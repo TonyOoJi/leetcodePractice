07a26bb5ea56b049749e389392a61d9b9f71abc4
tony <null>
2019-11-01 15:40:30 +0800

    lc-d2-[53]最大子序和

--- leetcode/editor/cn/[53]最大子序和.java
+++ leetcode/editor/cn/[53]最大子序和.java
@@ -17,8 +17,22 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxSubArray(int[] nums) {
+        // 遍历当加上一个新数 比新数字小的时候。说明前面的一串数字之和小于0了，则舍弃前面的 重新计算
 
-        return 1;
+        int sum = 0; //和
+        int maxSum = nums[0]; //当前最大和
+
+        for (int num : nums) {
+            if (sum + num > num) { //加下一个 大于下一个才保留 ，
+            //if (sum > 0) { // 表达式右侧移动到左侧减去num后：sum > 0
+                sum += num;
+            } else {
+                sum = num;
+            }
+            maxSum = Math.max(sum,maxSum);
+        }
+
+        return maxSum;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
